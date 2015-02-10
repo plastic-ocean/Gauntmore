84ad974061524e1cf16dff82ee3cda53ae6bb128
Benjamin <benjaminrbarnes@gmail.com>
2015-02-09 17:32:39 -0800

    Skeleton class added; needs methods to be defined

---
+++ Skeleton.cpp
@@ -0,0 +1,11 @@
+//
+//  Skeleton.cpp
+//  gauntmore_macosx
+//
+//  Created by benjamin barnes on 2/9/15.
+//  Copyright (c) 2015 oxygine. All rights reserved.
+//
+
+#include "Skeleton.h"
+
+
---
+++ Skeleton.h
@@ -0,0 +1,27 @@
+//
+//  Skeleton.h
+//  gauntmore_macosx
+//
+//  Created by benjamin barnes on 2/9/15.
+//  Copyright (c) 2015 oxygine. All rights reserved.
+//
+
+//#ifndef __gauntmore_macosx__Skeleton__
+//#define __gauntmore_macosx__Skeleton__
+
+//#include <stdio.h>
+
+#include "Unit.h"
+#include "Creature.h"
+
+DECLARE_SMART(Skeleton, spSkeleton);
+class Skeleton: public Creature {
+
+public:
+    
+private:
+    
+    
+};
+
+//#endif /* defined(__gauntmore_macosx__Skeleton__) */
