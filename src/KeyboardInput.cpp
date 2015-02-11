ce25a19da8fb888023cd5aa90e7cb13a0a07b110
David Wenzel <codenull@gmail.com>
2015-02-10 16:07:27 -0800

    input cleanup
    cleaned up keyboardInput.cpp, removed unused assets.
    added test sprite sheet

--- src/KeyboardInput.cpp
+++ src/KeyboardInput.cpp
@@ -28,50 +28,6 @@ bool KeyboardInput::getDirection(Vector2 &dir) const {
 }
 
 
-/**
- * Sets the movement direction for the Player on valid keyboard input.
- * 
- * @ev is the event, will be turned into an SDL_Event.
- */
-/*void KeyboardInput::_onEvent(Event *ev) {
-    SDL_Event *event = (SDL_Event*) ev->userData;
-    
-    if (event->type == SDL_KEYDOWN) {
-        const Uint8 *state = SDL_GetKeyboardState(NULL);
-        _pressed = true;
-        if ((state[SDL_SCANCODE_LEFT] && state[SDL_SCANCODE_UP]) || (state[SDL_SCANCODE_A] && state[SDL_SCANCODE_W])) {
-            // Up/Left
-            _dir = Vector2(-20, -20);
-        } else if ((state[SDL_SCANCODE_RIGHT] && state[SDL_SCANCODE_UP]) || (state[SDL_SCANCODE_D] && state[SDL_SCANCODE_W])) {
-            // Up/Right
-            _dir = Vector2(20, -20);
-        } else if ((state[SDL_SCANCODE_DOWN] && state[SDL_SCANCODE_LEFT]) || (state[SDL_SCANCODE_S] && state[SDL_SCANCODE_A])) {
-            // Down/Left
-            _dir = Vector2(-20, 20);
-        } else if ((state[SDL_SCANCODE_DOWN] && state[SDL_SCANCODE_RIGHT]) || (state[SDL_SCANCODE_S] && state[SDL_SCANCODE_D])) {
-            // Down/Right
-            _dir = Vector2(20, 20);
-        } else if (state[SDL_SCANCODE_UP] || state[SDL_SCANCODE_W]) {
-            // Up
-            _dir = Vector2(0, -20);
-        } else if (state[SDL_SCANCODE_DOWN] || state[SDL_SCANCODE_S]) {
-            // Down
-            _dir = Vector2(0, 20);
-        } else if (state[SDL_SCANCODE_LEFT] || state[SDL_SCANCODE_A]) {
-            // Left
-            _dir = Vector2(-20, 0);
-        } else if (state[SDL_SCANCODE_RIGHT] || state[SDL_SCANCODE_D]) {
-            // Right
-            _dir = Vector2(20, 0);
-        }
-    } else if (event->type == SDL_KEYUP) {
-        _pressed = false;
-        _dir = Vector2(0,0);
-    }
-    
-    
-}*/
-
 void KeyboardInput::_onEvent(Event *ev) {
     SDL_Event *event = (SDL_Event*) ev->userData;
 //    const Uint8 *state = SDL_GetKeyboardState(NULL);
@@ -110,25 +66,5 @@ void KeyboardInput::_onEvent(Event *ev) {
         }
     }
     
-  /*  if (state[SDL_SCANCODE_UP] || state[SDL_SCANCODE_W]) {
-        // Up
-        _dir += Vector2(0, -1);
-    }
-    if (state[SDL_SCANCODE_DOWN] || state[SDL_SCANCODE_S]) {
-        // Up
-        _dir += Vector2(0, 1);
-    }
-    if (state[SDL_SCANCODE_LEFT] || state[SDL_SCANCODE_A]) {
-        // Up
-        _dir += Vector2(-1, 0);
-    }
-    if (state[SDL_SCANCODE_RIGHT] || state[SDL_SCANCODE_D]) {
-        // Up
-        _dir += Vector2(1, 0);
-    }
-    if (event->type == SDL_KEYUP) {
-    _pressed = false;
-    _dir = Vector2(0,0);
-}
-*/
+ 
 }
